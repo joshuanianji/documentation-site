8e027d71a735e691d6b572ce795ba1013e2f7ccd
Joshua Ji <joshua.ji@jiwhiz.com>
2019-08-29 16:16:20 -0600

    finished table, but may change it later on

--- src/Page/Table.elm
+++ src/Page/Table.elm
@@ -4,7 +4,8 @@ module Page.Table exposing (Context, Model, Msg(..), init, update, view)
 -}
 
 
-import Element exposing (Color, Element, fill, height, width)
+import Element exposing (Color, Element, fill, height, width, spacing)
+import Element.Font as Font
 import Routes
 import SharedState exposing (SharedState, SharedStateUpdate(..))
 import UiFramework exposing (UiContextual, WithContext, toElement)
@@ -13,7 +14,7 @@ import UiFramework.Table as Table
 import UiFramework.Types exposing (Role(..))
 import UiFramework.Typography as Typography
 import Util
-import View.Component exposing (componentNavbar, viewHeader)
+import View.Component as Component exposing (componentNavbar, viewHeader)
 
 
 
@@ -86,13 +87,224 @@ content : UiElement Msg
 content =
     UiFramework.uiColumn
         [ width fill
-        , Element.spacing 64
+        , spacing 64
         ]
-        [ Typography.h1 [] (Util.text "Module Code")
+        [ basicExample
+        , styles
+        ]
+
+basicExample : UiElement Msg
+basicExample =
+    UiFramework.uiColumn
+        [ width fill
+        , Element.spacing 32
+        ]
+        [ Component.title "Basic Example"
+        , Component.wrappedText "The table module does not have a \"Table.default\" function - you must build up from the Table.simpleTable. Nevertheless, here is a basic example with hardcoded data."
+        , Table.simpleTable
+            |> Table.withColumns tableColumn
+            |> Table.view information
+        , basicExampleCode
         ]
 
 
+basicExampleCode : UiElement Msg
+basicExampleCode =
+    """
+import UiFramework.Table as Table
 
+
+simpleTable =
+    Table.simpleTable
+        |> Table.withColumns tableColumn
+        |> Table.view information
+
+
+information =
+    [ { role = "Row 1"
+        , column1 = "column 1"
+        , column2 = "column 2"
+        , column3 = "column 3"
+        }
+    , { role = "Row 2"
+        , column1 = "column 1"
+        , column2 = "column 2"
+        , column3 = "column 3"
+        }
+    , { role = "Row 3"
+        , column1 = "column 1"
+        , column2 = "column 2"
+        , column3 = "column 3"
+        }
+    , { role = "Row 4"
+        , column1 = "column 1"
+        , column2 = "column 2"
+        , column3 = "column 3"
+        }
+    ]
+
+
+tableColumn =
+    [ { head = Util.text "Role"
+        , viewData = \\data -> UiFramework.uiParagraph [ Font.bold ] [ Util.text data.role ]
+        }
+    , { head = Util.text "Column 1"
+        , viewData = \\data -> UiFramework.uiParagraph [] [ Util.text data.column1 ]
+        }
+    , { head = Util.text "Column 2"
+        , viewData = \\data -> UiFramework.uiParagraph [] [ Util.text data.column2 ]
+        }
+    , { head = Util.text "Column 3"
+        , viewData = \\data -> UiFramework.uiParagraph [] [ Util.text data.column3 ]
+        }
+    ]"""     
+        |> Util.uiHighlightCode "elm"
+
+
+styles : UiElement Msg 
+styles =
+    UiFramework.uiColumn
+        [ width fill
+        , Element.spacing 32
+        ]
+        [ Component.title "Styles"
+        , Component.wrappedText "There are a variety of styling configurations one can choose from when making a table."
+        , stripedConfig
+        , borderConfig
+        , compactConfig
+        ]
+
+stripedConfig : UiElement Msg 
+stripedConfig =
+    UiFramework.uiColumn
+        [ width fill
+        , Element.spacing 32
+        ]
+        [ Component.section "Striped tables"
+        , Component.wrappedText "Tables can be striped with little extra code. So far, all tables are striped though lol."
+        , Table.simpleTable
+            |> Table.withColumns tableColumn
+            |> Table.withStriped
+            |> Table.view information
+        , stripedConfigCode
+        ]
+    
+stripedConfigCode : UiElement Msg 
+stripedConfigCode =
+    """
+simpleTable =
+    Table.simpleTable
+        |> Table.withStriped
+        |> Table.withColumns tableColumn
+        |> Table.view information"""
+    |> Util.uiHighlightCode "elm"
+
+
+borderConfig : UiElement Msg 
+borderConfig =
+    UiFramework.uiColumn
+        [ width fill
+        , Element.spacing 32
+        ]
+        [ Component.section "Bordered and Borderless"
+        , Component.wrappedText "Table borders can be configured. Below is a borderless one."
+        , Table.simpleTable
+            |> Table.withBorderless
+            |> Table.withColumns tableColumn
+            |> Table.view information
+        , borderLessCode
+        , Component.wrappedText "And below is one with all borders."
+        , Table.simpleTable
+            |> Table.withBordered
+            |> Table.withColumns tableColumn
+            |> Table.view information
+        , borderedCode
+        ]
+    
+borderLessCode : UiElement Msg 
+borderLessCode =
+    """
+simpleTable =
+    Table.simpleTable
+        |> Table.withBorderless
+        |> Table.withColumns tableColumn
+        |> Table.view information"""
+    |> Util.uiHighlightCode "elm"
+
+
+borderedCode : UiElement Msg 
+borderedCode =
+    """
+simpleTable =
+    Table.simpleTable
+        |> Table.withBordered
+        |> Table.withColumns tableColumn
+        |> Table.view information"""
+    |> Util.uiHighlightCode "elm"
+
+
+compactConfig : UiElement Msg 
+compactConfig =
+    UiFramework.uiColumn
+        [ width fill
+        , Element.spacing 32
+        ]
+        [ Component.section "Compact tables"
+        , Table.simpleTable
+            |> Table.withCompact
+            |> Table.withColumns tableColumn
+            |> Table.view information
+        , compactConfigCode
+        ]
+    
+compactConfigCode : UiElement Msg 
+compactConfigCode =
+    """
+simpleTable =
+    Table.simpleTable
+        |> Table.withCompact
+        |> Table.withColumns tableColumn
+        |> Table.view information"""
+    |> Util.uiHighlightCode "elm"
+
+-- table information
+tableColumn =
+                [ { head = Util.text "Role"
+                  , viewData = \data -> UiFramework.uiParagraph [ Font.bold ] [ Util.text data.role ]
+                  }
+                , { head = Util.text "Column 1"
+                  , viewData = \data -> UiFramework.uiParagraph [] [ Util.text data.column1 ]
+                  }
+                , { head = Util.text "Column 2"
+                  , viewData = \data -> UiFramework.uiParagraph [] [ Util.text data.column2 ]
+                  }
+                , { head = Util.text "Column 3"
+                  , viewData = \data -> UiFramework.uiParagraph [] [ Util.text data.column3 ]
+                  }
+                ]
+
+information =
+                [ { role = "Row 1"
+                  , column1 = "column 1"
+                  , column2 = "column 2"
+                  , column3 = "column 3"
+                  }
+                , { role = "Row 2"
+                  , column1 = "column 1"
+                  , column2 = "column 2"
+                  , column3 = "column 3"
+                  }
+                , { role = "Row 3"
+                  , column1 = "column 1"
+                  , column2 = "column 2"
+                  , column3 = "column 3"
+                  }
+                , { role = "Row 4"
+                  , column1 = "column 1"
+                  , column2 = "column 2"
+                  , column3 = "column 3"
+                  }
+                ]
 -- UPDATE
 
 
